9b7f8f6b7020fa75eb54a9dab7e4a5acbac0cf7e
aleksej <xfrbt1@gmail.com>
2023-07-31 18:50:53 +0300

    Initial commit

--- plots_constructor/plot_3D.py
+++ plots_constructor/plot_3D.py
@@ -10,15 +10,16 @@ class Plot_3D:
     def plot(self, x, y, z):
         self.ax.plot3D(x, y, z, color='red')
 
+    def plot_surface(self, x, y, z):
+        self.ax.plot_surface(x, y, z)
+
     def set_labels(self, xl, yl, zl):
         self.ax.set_xlabel(xl)
         self.ax.set_ylabel(yl)
         self.ax.set_zlabel(zl)
 
-    def set_colors(self, colors):
-        self.colors = colors
-
     @staticmethod
     def show():
         plt.show()
+
 
